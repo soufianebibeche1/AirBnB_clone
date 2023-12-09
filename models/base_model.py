a0bb42ad2b38c1b208970bc496c89aace76be8e7
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-12-09 15:30:49 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -9,29 +9,13 @@ class BaseModel:
     """
     BaseModel class definition.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
         Initializes an instance of BaseModel.
-        Args:
-            *args: Unused
-            **kwargs: Dictionary of attributes to initialize the instance
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-            if 'id' not in kwargs:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if 'created_at' not in kwargs:
-                setattr(self, 'created_at', datetime.now())
-            if 'updated_at' not in kwargs:
-                setattr(self, 'updated_at', datetime.now())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -43,15 +27,12 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime.
+        Updates updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
-        A key __class__ is added to this dictionary with the class name of the object.
-        created_at and updated_at are converted to string object in ISO format.
         Returns:
             Dictionary representation of the instance
         """
