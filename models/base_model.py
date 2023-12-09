ec5b034c67f9d0624dd9244ef178f3379ad20ff9
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-12-09 13:54:27 -0800

    testm

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Module contain the BaseModel class.
+Module containing the BaseModel class.
 """
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     """
-    BaseModel class.
+    BaseModel class for common attributes/methods for other classes.
     """
 
     def __init__(self, *args, **kwargs):
@@ -32,6 +31,8 @@ class BaseModel:
 
     def __str__(self):
         """
+        Returns a string representation of the BaseModel instance.
+
         Returns:
             str: String representation of the BaseModel instance.
         """
@@ -40,12 +41,15 @@ class BaseModel:
 
     def save(self):
         """
-        Updates updated_at with the current datetime.
+        Updates the public instance attribute updated_at
+        with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
+        Returns a dictionary containing all keys/values of __dict__.
+
         Returns:
             dict: A dictionary representation of the BaseModel instance.
         """
