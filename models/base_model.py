e97c1d4b5074104967cab614f5e2c235727972c6
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-12-10 08:15:33 -0800

    tetsm

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 # models/base_model.py
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -17,6 +17,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -25,6 +26,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
