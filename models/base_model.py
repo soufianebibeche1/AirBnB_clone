a7d27f51665615eaad66db8b30a41241ef69cc1d
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-12-10 07:32:47 -0800

    testing update

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,19 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
